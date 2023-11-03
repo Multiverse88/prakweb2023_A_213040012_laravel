a16e3e2e06282966d29c7dac26b2e51003774109
Multiverse88 <ainanihj@gmail.com>
2023-11-03 21:28:48 +0700

    Video ke 5 models, collection dan controllers

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,5 @@
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
     <img src="img/{{ $image }}" alt="{{ $name }}" width="
-    200">    
-@endsection
+    200" class="img-thumbnail rounded-circle">    
+@endsections
