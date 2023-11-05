b2ad4b1e024e02cc9299428567d4d327fd764800
Multiverse88 <ainanihj@gmail.com>
2023-11-05 12:53:21 +0700

    Video 7 Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,33 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    private static $blog_post = [
-        [
-            'title' => "Judul Post Pertama",
-            'slug' => "judul-post-pertama",
-            'author' => "Ainan Bahrul Ihsan",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur a illo ipsam commodi tenetur rerum cupiditate. Exercitationem, labore voluptatum ipsum cum excepturi dolor iure sint consequatur numquam libero ex laudantium sit ipsa esse nisi, dolorum expedita possimus beatae, maxime veniam a pariatur eos impedit. Incidunt mollitia laboriosam dolorum odio harum ratione, quo, quod quibusdam expedita asperiores fugit exercitationem voluptatum ex rerum neque, suscipit at eveniet corrupti repellat accusamus dicta! Sequi accusamus voluptatum voluptate, aut animi provident laboriosam molestias incidunt ullam."
-        ],
-
-        [
-            'title' => "Judul Post afandi",
-            'slug' => "judul-post-kedua",
-            'author' => "M Afandi",
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde provident aperiam ut vero odit voluptatibus quaerat amet velit, labore eos corporis dolor consequuntur est iusto omnis! Sequi sint impedit quis ullam pariatur, voluptatum officiis? In recusandae velit corrupti non dolore nesciunt sequi temporibus sed eveniet iste, commodi officiis quia, dolores ullam libero aliquam sint ipsum veritatis maxime aut necessitatibus? Obcaecati architecto saepe minima magnam, odio aliquid corrupti itaque aliquam dolorum fugit sunt natus enim sit asperiores ullam. Debitis dolor numquam reiciendis tenetur perspiciatis aliquam ab ipsum, placeat nemo modi error ipsa cumque molestiae quae nulla officia id maxime consequatur alias."
-        ]
-    ];
-
-    public static function all(){
-        return collect(self::$blog_post);
-    }
-
-    public static function find($slug) {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,4 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index']);
 
 // Single Post
-Route::get('posts/{slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
