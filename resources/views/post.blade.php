2360bb5301f392e259dbe248de5874e34bb7cc51
Multiverse88 <ainanihj@gmail.com>
2023-11-05 16:32:50 +0700

    Video 8 Post Category dan Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,11 @@
 
 @section('container')
     <article>
-        <h2>{{ $post->title}}</h2>
+        <h1 class="mb-5">{{ $post->title}}</h1>
+
+        <p>By. Ainan in <a href="/categories/{{ $post->category->slug}}">{{ 
+            $post->category->name}}</a></p>
+
         {!! $post->body !!}
     </article>
     
