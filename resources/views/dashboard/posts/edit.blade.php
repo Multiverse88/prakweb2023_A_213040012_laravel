38b5294532b908d52312ca8def96558bc0808630
Multiverse88 <ainanihj@gmail.com>
2023-11-27 23:13:54 +0700

    Video 22 Preview, Update & Delete Image

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -7,9 +7,10 @@
 </div>
 
 <div class="col-lg-8">
-  <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+  <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
     @method('put')
     @csrf
+    <!-- title -->
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
       <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title', $post->title) }}">
@@ -20,6 +21,7 @@
       </div>
       @enderror
     </div>
+    <!-- slug -->
     <div class="mb-3">
       <label for="slug" class="form-label">Slug</label>
       <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug', $post->slug) }}">
@@ -29,6 +31,7 @@
       </div>
       @enderror
     </div>
+    <!-- category -->
     <div class="mb-3">
       <label for="category" class="form-label">Category</label>
       <select class="form-select" name="category_id">
@@ -40,6 +43,21 @@
         @endif
         @endforeach
       </select>
+    </div>
+    <!-- image -->
+    <div class="mb-3">
+      <label for="formFile" class="form-label">Post Image</label>
+      <input type="hidden" name="oldImage" value="{{ $post->image }}">
+      @if ($post->image)
+      <img src="{{asset('storage/'. $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+      @else
+      <img class="img-preview img-fluid mb-3 col-sm-5">
+      @endif
+
+      <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+      @error('image')
+      <p class="text-danger"> {{ $message }}</p>
+      @enderror
     </div>
     <div class="mb-3">
       <label for="body" class="form-label"></label>
@@ -69,5 +87,19 @@
   document.addEventListener('trix-file-accept', function(e) {
     e.preventDefault();
   });
+
+  function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
+  }
 </script>
 @endsection
